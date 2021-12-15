858f80ecd1ceb00abfc1fa7948f2cdafc53cfc13
NastyaBondarenko <av_bondarenko@rambler.ru>
2021-12-15 22:16:19 +0200

    reflection

---
+++ src/test/java/com/bondarenko/testreflection/testClasses/TestClassWithMethodsWithoutParameters.java
@@ -0,0 +1,35 @@
+package com.bondarenko.testreflection.testClasses;
+
+public class TestClassWithMethodsWithoutParameters {
+
+    private int a = 5;
+
+    public void method1() {
+
+    }
+
+    public void method2(Object object) {
+
+    }
+
+    public void method3() {
+
+    }
+
+    private void method4(Object object) {
+
+    }
+
+    public final void method5() {
+
+    }
+
+    public void method6() {
+
+    }
+
+    private void method7(final Object object) {
+
+    }
+
+}
